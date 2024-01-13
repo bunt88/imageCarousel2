20d2b2a24513175de3ec7b5a6082e405d8631366
CBunting <bunt88@gmail.com>
2024-01-13 15:02:15 -0700

    Created Files and added Swiper

---
+++ my-gallery.js
@@ -0,0 +1,8 @@
+new Swiper(".swiper-container", {
+    speed: 400, 
+    spaceBetween: 200,
+    navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev"
+    }
+});
